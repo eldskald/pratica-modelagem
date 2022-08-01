835ac317fdaa7efdae5a69fe8696b4db7d94d456
Rafael Bordoni <49042948+eldskald@users.noreply.github.com>
2022-08-01 19:53:52 -0300

    Update modelagem.sql

--- modelagem.sql
+++ modelagem.sql
@@ -1,46 +1,48 @@
-CREATE TABLE "public.users" (
-	"id" serial NOT NULL,
+CREATE DATABASE "pratica_modelagem";
+
+
+
+CREATE TABLE "addresses" (
+	"id" serial PRIMARY KEY,
+	"address" TEXT NOT NULL,
+	"complement" TEXT,
+	"city" TEXT NOT NULL,
+	"stateOrProvince" TEXT,
+	"country" TEXT NOT NULL,
+	"cep" integer NOT NULL
+);
+
+
+
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY,
 	"name" TEXT NOT NULL,
 	"email" TEXT NOT NULL UNIQUE,
 	"password" TEXT NOT NULL,
-	"cartId" integer NOT NULL,
-	"address1" integer NOT NULL,
-	"address2" integer NOT NULL,
-	"address3" integer NOT NULL,
-	"address4" integer NOT NULL,
-	CONSTRAINT "users_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"address1" integer NOT NULL REFERENCES "addresses"("id"),
+	"address2" integer NOT NULL REFERENCES "addresses"("id"),
+	"address3" integer NOT NULL REFERENCES "addresses"("id"),
+	"address4" integer NOT NULL REFERENCES "addresses"("id")
 );
 
 
 
-CREATE TABLE "public.products" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	"price" integer NOT NULL,
-	"categoryId" integer NOT NULL,
-	"sizeId" integer NOT NULL,
-	"picturesId" integer NOT NULL,
-	CONSTRAINT "products_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "categories" (
+	"id" serial PRIMARY KEY,
+	"name" TEXT NOT NULL
 );
 
 
 
-CREATE TABLE "public.categories" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	CONSTRAINT "categories_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "sizes" (
+	"id" serial PRIMARY KEY,
+	"name" TEXT NOT NULL
 );
 
 
 
-CREATE TABLE "public.pictures" (
-	"id" serial NOT NULL,
+CREATE TABLE "pictures" (
+	"id" serial PRIMARY KEY,
 	"mainPicture" TEXT NOT NULL,
 	"aPicture" TEXT NOT NULL,
 	"bPicture" TEXT NOT NULL,
@@ -48,89 +50,35 @@ CREATE TABLE "public.pictures" (
 	"dPicture" TEXT NOT NULL,
 	"ePicture" TEXT NOT NULL,
 	"fPicture" TEXT NOT NULL,
-	"gPicture" TEXT NOT NULL,
-	CONSTRAINT "pictures_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"gPicture" TEXT NOT NULL
 );
 
 
 
-CREATE TABLE "public.sizes" (
-	"id" serial NOT NULL,
+CREATE TABLE "products" (
+	"id" serial PRIMARY KEY,
 	"name" TEXT NOT NULL,
-	CONSTRAINT "sizes_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"price" integer NOT NULL,
+	"categoryId" integer NOT NULL REFERENCES "categories"("id"),
+	"sizeId" integer NOT NULL REFERENCES "sizes"("id"),
+	"picturesId" integer NOT NULL REFERENCES "pictures"("id")
 );
 
 
 
-CREATE TABLE "public.purchases" (
-	"id" serial NOT NULL,
+CREATE TABLE "purchaseStatuses" (
+	"id" serial PRIMARY KEY,
+	"name" TEXT NOT NULL,
+	"lastUpdate" TIMESTAMP NOT NULL
+);
+
+
+
+CREATE TABLE "purchases" (
+	"id" serial PRIMARY KEY,
 	"quantity" integer NOT NULL,
-	"userId" integer NOT NULL,
-	"productId" integer NOT NULL,
-	"deliveryAddress" integer NOT NULL,
-	"statusId" integer NOT NULL,
-	CONSTRAINT "purchases_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"userId" integer NOT NULL REFERENCES "users"("id"),
+	"productId" integer NOT NULL REFERENCES "products"("id"),
+	"deliveryAddress" integer NOT NULL REFERENCES "addresses"("id"),
+	"statusId" integer NOT NULL REFERENCES "purchaseStatuses"("id")
 );
-
-
-
-CREATE TABLE "public.purchaseStatuses" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
-	"lastUpdate" TIMESTAMP NOT NULL,
-	CONSTRAINT "purchaseStatuses_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "public.addresses" (
-	"id" serial NOT NULL,
-	"address" TEXT NOT NULL,
-	"complement" TEXT,
-	"city" TEXT NOT NULL,
-	"stateOrProvince" TEXT,
-	"country" TEXT NOT NULL,
-	"cep" integer NOT NULL,
-	CONSTRAINT "addresses_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-ALTER TABLE "users" ADD CONSTRAINT "users_fk0" FOREIGN KEY ("cartId") REFERENCES "shoppingCarts"("id");
-ALTER TABLE "users" ADD CONSTRAINT "users_fk1" FOREIGN KEY ("address1") REFERENCES "addresses"("id");
-ALTER TABLE "users" ADD CONSTRAINT "users_fk2" FOREIGN KEY ("address2") REFERENCES "addresses"("id");
-ALTER TABLE "users" ADD CONSTRAINT "users_fk3" FOREIGN KEY ("address3") REFERENCES "addresses"("id");
-ALTER TABLE "users" ADD CONSTRAINT "users_fk4" FOREIGN KEY ("address4") REFERENCES "addresses"("id");
-
-ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("categoryId") REFERENCES "categories"("id");
-ALTER TABLE "products" ADD CONSTRAINT "products_fk1" FOREIGN KEY ("sizeId") REFERENCES "sizes"("id");
-ALTER TABLE "products" ADD CONSTRAINT "products_fk2" FOREIGN KEY ("picturesId") REFERENCES "pictures"("id");
-
-
-
-
-ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
-ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk1" FOREIGN KEY ("productId") REFERENCES "products"("id");
-ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk2" FOREIGN KEY ("deliveryAddress") REFERENCES "addresses"("id");
-ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk3" FOREIGN KEY ("statusId") REFERENCES "purchaseStatuses"("id");
-
-
-
-
-
-
-
-
-
-
-
